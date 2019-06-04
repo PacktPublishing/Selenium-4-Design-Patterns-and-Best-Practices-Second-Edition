e620e077d27569eee9b78ec27ad70c08c3ca4817
angelov.st.anton <angelov.anton@yahoo.com>
2019-06-04 10:35:32 +0300

    short name of folder

--- Chapter 9-Assessment System/AssessmentSystemDemos/FacadeDesignPattern/v.0.1/FacadeTests.cs
+++ Chapter 9-Assessment System/AssessmentSystemDemos/FacadeDesignPattern/v.0.1/FacadeTests.cs
@@ -29,60 +29,6 @@ namespace AssessmentSystemDemos.Facades.First
         }
 
         [TestMethod]
-        public void PurchaseFalcon9WithoutFacade()
-        {
-            _mainPage.Open();
-            _mainPage.AddRocketToShoppingCart("Falcon 9");
-            _cartPage.ApplyCoupon("happybirthday");
-            _cartPage.CartPageAssertions.AssertCouponAppliedSuccessfully();
-            _cartPage.IncreaseProductQuantity(2);
-            _cartPage.CartPageAssertions.AssertTotalPrice("114.00€");
-            _cartPage.CartPageElements.ProceedToCheckout.Click();
-
-            var purchaseInfo = new PurchaseInfo()
-                               {
-                                   FirstName = "Anton",
-                                   LastName = "Angelov",
-                                   Company = "Space Flowers",
-                                   Country = "Germany",
-                                   Address1 = "1 Willi Brandt Avenue Tiergarten",
-                                   Address2 = "Lützowplatz 17",
-                                   City = "Berlin",
-                                   Zip = "10115",
-                                   Phone = "+00498888999281",
-                               };
-            _checkoutPage.FillBillingInfo(purchaseInfo);
-            _checkoutPage.CheckoutPageAssertions.AssertOrderReceived();
-        }
-
-        [TestMethod]
-        public void PurchaseSaturnVWithoutFacade()
-        {
-            _mainPage.Open();
-            _mainPage.AddRocketToShoppingCart("Saturn V");
-            _cartPage.ApplyCoupon("happybirthday");
-            _cartPage.CartPageAssertions.AssertCouponAppliedSuccessfully();
-            _cartPage.IncreaseProductQuantity(3);
-            _cartPage.CartPageAssertions.AssertTotalPrice("355.00€");
-            _cartPage.CartPageElements.ProceedToCheckout.Click();
-
-            var purchaseInfo = new PurchaseInfo()
-                               {
-                                   FirstName = "John",
-                                   LastName = "Atanasov",
-                                   Company = "Space Flowers",
-                                   Country = "Germany",
-                                   Address1 = "1 Willi Brandt Avenue Tiergarten",
-                                   Address2 = "Lützowplatz 17",
-                                   City = "Berlin",
-                                   Zip = "10115",
-                                   Phone = "+00498888999281",
-                               };
-            _checkoutPage.FillBillingInfo(purchaseInfo);
-            _checkoutPage.CheckoutPageAssertions.AssertOrderReceived();
-        }
-
-        [TestMethod]
         public void PurchaseFalcon9WithFacade()
         {
             var purchaseInfo = new PurchaseInfo()
