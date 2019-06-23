3059c8771c2d2819520bb68cd184bc089cecbd90
angelov.st.anton <angelov.anton@yahoo.com>
2019-06-23 17:36:42 +0300

    example data driven test

--- Chapter 10-Test Data Preparation/TestDataPreparationDemos/FacadeDesignPattern/v.0.2/FacadeTests.cs
+++ Chapter 10-Test Data Preparation/TestDataPreparationDemos/FacadeDesignPattern/v.0.2/FacadeTests.cs
@@ -11,6 +11,13 @@ namespace TestDataPreparationDemos.Facades.Second
         private static CheckoutPage _checkoutPage;
         private static PurchaseFirstVersionFacade _purchaseFirstVersionFacade;
 
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
@@ -65,5 +72,27 @@ namespace TestDataPreparationDemos.Facades.Second
 
             _purchaseFirstVersionFacade.PurchaseItem("Saturn V", "happybirthday", 3, "355.00€", purchaseInfo);
         }
+
+        // .NET Core does not support the DataSource attribute. If you try to access test data in this way in a .NET Core or UWP unit test project, 
+        // you'll see an error similar to "'TestContext' does not contain a definition for 'DataRow' and no accessible extension method 'DataRow'
+        // accepting a first argument of type 'TestContext' could be found (are you missing a using directive or an assembly reference?)".
+        ////[TestMethod]
+        ////[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "TestsData.csv", "TestsData#csv", DataAccessMethod.Sequential)]
+        ////public void PurchaseWithFacadeDataDrivenCsv()
+        ////{
+        ////    var purchaseInfo = new PurchaseInfo();
+
+        ////    string rocketName = TestContext.DataRow["rocketName"];
+        ////    string couponName = TestContext.DataRow["couponName"];          
+        ////    int quantity = int.Parse(this.TestContext.DataRow["quantity"]);
+        ////    string expectedPrice = TestContext.DataRow["expectedPrice"];
+
+        ////    _purchaseFirstVersionFacade.PurchaseItem(
+        ////        rocketName,
+        ////        couponName,
+        ////        quantity,
+        ////        expectedPrice,
+        ////        purchaseInfo);
+        ////}
     }
 }
