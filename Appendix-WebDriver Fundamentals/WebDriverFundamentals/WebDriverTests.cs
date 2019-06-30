967a9cf6784a14c9eaf17e90b8c0bfe7030475e0
Anton Angelov <angelov.anton@yahoo.com>
2019-06-30 15:39:54 +0300

    add new webdrive fundamentals appendix examples

---
+++ Appendix-WebDriver Fundamentals/WebDriverFundamentals/WebDriverTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestDataPreparationDemos
+{
+    [TestClass]
+    public class WebDriverTests
+    {
+        private IWebDriver _driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _driver.Quit();
+        }
+
+        [TestMethod]
+        public void CompletePurchaseSuccessfully_WhenNewClient()
+        {
+            _driver.Navigate().GoToUrl("http://demos.bellatrix.solutions/");
+
+            var addToCartFalcon9 = _driver.FindElement(By.CssSelector("[data-product_id*='28']"));
+            addToCartFalcon9.Click();
+            Thread.Sleep(500);
+            var viewCartButton = _driver.FindElement(By.CssSelector("[class*='added_to_cart wc-forward']"));
+            viewCartButton.Click();
+
+            var couponCodeTextField = _driver.FindElement(By.Id("coupon_code"));
+            couponCodeTextField.Clear();
+            couponCodeTextField.SendKeys("happybirthday");
+            var applyCouponButton = _driver.FindElement(By.CssSelector("[value*='Apply coupon']"));
+            applyCouponButton.Click();
+            Thread.Sleep(2000);
+            var messageAlert = _driver.FindElement(By.CssSelector("[class*='woocommerce-message']"));
+            Assert.AreEqual("Coupon code applied successfully.", messageAlert.Text);
+
+            var quantityBox = _driver.FindElement(By.CssSelector("[class*='input-text qty text']"));
+            quantityBox.Clear();
+            Thread.Sleep(50);
+            quantityBox.SendKeys("2");
+
+            Thread.Sleep(2000);
+            var updateCart = _driver.FindElement(By.CssSelector("[value*='Update cart']"));
+            updateCart.Click();
+            Thread.Sleep(2000);
+            var totalSpan = _driver.FindElement(By.XPath("//*[@class='order-total']//span"));
+            Assert.AreEqual("114.00€", totalSpan.Text);
+        }
+    }
+}
